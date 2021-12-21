d43aa7cd45f6bdcab380d43433f4e27b968b0671
Fabio-web <fabio.maccotta@ynov.com>
2021-12-21 18:45:44 +0100

    + add home page

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_banking_app/constants/colors.dart';
+import 'package:mobile_banking_app/screens/home/widgets/catch_phrase.dart';
+import 'package:mobile_banking_app/widgets/button_builder.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,10 +13,48 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello World !', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
-      ),
+        backgroundColor: kBackground,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(vertical: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 40, right: 40, top: MediaQuery.of(context).padding.top),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 300,
+                      child: Stack(
+                        children: [
+                          Center(
+                            child: ClipRRect(
+                              child: Image.asset('assets/images/card2.png'),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CatchPhrase(),
+                        Column(
+                          children: [
+                            ButtonBuilder("Sign up", backgroundColor: kSecondaryColor),
+                            SizedBox(height: 15),
+                            ButtonBuilder("Log in", backgroundColor: Colors.transparent, color: Colors.black),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        )
     );
   }
-
 }
