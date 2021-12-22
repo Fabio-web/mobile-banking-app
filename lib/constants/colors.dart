544012c5e1bd14e08f09bda6ebdd89ac3c17fbd3
Fabio-web <fabio.maccotta@ynov.com>
2021-12-22 02:19:04 +0100

    add some constants colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const Color kBackground = Color(0xFFFDFDFD);
 const Color kPrimaryColor = Color(0xFF38c1ff);
 const Color kSecondaryColor = Color(0xFF12d488);
+const Color kdGrey = Color(0xFF979797);
+const Color kGrey = Color(0xFFadadad);
